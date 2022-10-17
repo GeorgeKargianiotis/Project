2a935d62f145a9556ad4f1cf5165828aff246f44
bill <gravityman12@hotmail.com>
2022-10-17 19:28:01 +0300

    test

---
+++ createMakefile.sh
@@ -0,0 +1,2 @@
+cgal_create_CMakeLists -s main
+cmake -DCGAL_DIR=$HOME/CGAL-5.5.1/lib/CGAL -DCMAKE_BUILD_TYPE=Release
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <CGAL/Simple_cartesian.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_2 Point_2;
+
+int main() {
+
+
+	return 0;
+}

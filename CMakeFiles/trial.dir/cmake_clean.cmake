583c08d9a9098745983060ddb77074e53f39d9be
bill <gravityman12@hotmail.com>
2022-10-17 19:16:48 +0300

    test 2

---
+++ CMakeFiles/trial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trial.dir/trial.cpp.o"
+  "trial"
+  "trial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

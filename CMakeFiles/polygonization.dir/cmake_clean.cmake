117203dd694d167176f8063174c7be976ca91acd
GeorgeKargianiotis <sdi1900075@di.uoa.gr>
2022-10-19 18:37:30 +0300

    new branch

---
+++ CMakeFiles/polygonization.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polygonization.dir/main.cpp.o"
+  "CMakeFiles/polygonization.dir/polygonization.cpp.o"
+  "polygonization"
+  "polygonization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/polygonization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

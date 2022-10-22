2322337084befdad4c272882ec70f87f40593f68
bill <gravityman12@hotmail.com>
2022-10-22 17:55:14 +0300

    fix splitString function

--- sources/utils.cpp
+++ sources/utils.cpp
@@ -9,8 +9,8 @@ std::vector<std::string> utils::splitString(std::string str, char delimiter = ' 
 	int begin = 0;
 	int end = str.find(delimiter);
 	while(end != -1){
-		split.push_back(str.substr(begin, end-begin));
-		begin = ++end;
+		split.push_back(str.substr(begin, end - begin));
+		begin = end + 1;
 		end = str.find(delimiter, begin);
 	}
 

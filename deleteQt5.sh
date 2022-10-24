124abc9d519ac88b4eee6173ab052bdc990ce149
bill <gravityman12@hotmail.com>
2022-10-24 16:32:00 +0300

    remove script

--- deleteQt5.sh
+++
@@ -1,9 +0,0 @@
-rm -r CGAL_Qt5_moc_and_resources_autogen
-rm -r resources
-rm -r icons
-rm libCGAL_Qt5_moc_and_resources.a
-rm qrc_CGAL.cpp
-rm qrc_File.cpp
-rm qrc_Input.cpp
-rm qrc_Triangulation_2.cpp
-rm ui_ImageInterface.h

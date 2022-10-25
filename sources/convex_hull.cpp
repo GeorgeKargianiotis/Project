29207e952dcec73d8a307c4eef3de26a3aa3be4e
GeorgeKargianiotis <sdi1900075@di.uoa.gr>
2022-10-25 22:51:45 +0300

    Progress Algorithm

--- sources/convex_hull.cpp
+++ sources/convex_hull.cpp
@@ -3,44 +3,41 @@
 #include "../headers/convex_hull.hpp"
 #include "../headers/cgalConfig.hpp"
 
+typedef std::vector<Point_2>::iterator pveciterator;
+typedef std::vector<Segment_2> Segments; 
+
 void convex_hull::convex_HullAlgorithm(std::vector<Point_2> &Points){	
 
-	Polygon_2 mypolygon, chp;
-
-	// Take the first three points and use them as starting ΚΠ
-	// Edge cases?
-	for(auto point : Points)
-		std::cout << point << std::endl;
-	mypolygon.push_back(Points[0]);
-	mypolygon.push_back(Points[1]);
-	mypolygon.push_back(Points[2]);
+	Polygon_2 mypolygon, polygonchain;
+	Segments myseg;
 
 	// Needed for points to add
-	int counter = 2;
-
-	
+	int counter = 0;
 
 	// Πολυγωνική Αλυσίδα:vector<Segment_2> 
 	// ΚΠ: CONVEX_HULL_2
 	const Polygon_2::Vertices& range = mypolygon.vertices();
  	std::vector<Point_2> result;
 
- 	for(auto it = range.begin(); it!= range.end(); ++it){
- 	std::cout << *it << std::endl;
- 	}
 
-	for(const Segment_2& e : mypolygon.edges()){
- 	std::cout << e << std::endl;
- 	}
- 	std::cout << mypolygon.area() << std::endl;
-
-
+	// Using all given points to create initial chain
 	CGAL::convex_hull_2(range.begin(), range.end(), std::back_inserter(result));
 
- 	for(auto it = result.begin(); it!= result.end();++it)
- 		chp.push_back(*it);
+ 	for (auto it = result.begin(); it!= result.end();++it)
+ 		polygonchain.push_back(*it);
+	std::cout << result.size() << " points on the convex hull" << std::endl;
 
- 	std::cout << chp.area() << std::endl;
+	for (pveciterator iter=result.begin(); iter!=result.end(); ++iter)
+ 		std::cout << *iter << std::endl;
+ 	
+
+ 	std::cout << polygonchain.area() << std::endl;
+
+	// Make edges from polygon chain, add them to vector
+	for (auto vi = polygonchain.vertices_begin(); vi != polygonchain.vertices_end(); vi+=2)
+		myseg.push_back(Segment_2 (*vi, *(vi+1)));
+
+	// From each edge, find nearest visible point and add it to polygon	
 
  	const bool simpl = mypolygon.is_simple();
  	if (simpl) 

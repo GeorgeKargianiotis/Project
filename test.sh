18a1d517c13eafdd0388ba9cb8111c90bb42131a
GeorgeKargianiotis <sdi1900075@di.uoa.gr>
2022-11-05 17:02:03 +0200

    Tests

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
-./polygonization -i euro-night-0000200.instance -o output.txt -algorithm convex_hull -edge_selection 3 -initialization 2a
+./polygonization -i uniform-0000050-1.instance -o output.txt -algorithm convex_hull -edge_selection 1 -initialization 2a
 #./polygonization -i ../images/euro-night-000./0200.instance -o o -algorithm incremental -edge_selection 1 -initialization 1a 
 
 # Triangle(p1, p2, p3);

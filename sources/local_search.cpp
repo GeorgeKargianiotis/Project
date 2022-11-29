ce2b1684b57bfff44c9d705d0b51b13aafa05ef3
GeorgeKargianiotis <sdi1900075@di.uoa.gr>
2022-11-30 00:34:34 +0200

    Process

--- sources/local_search.cpp
+++ sources/local_search.cpp
@@ -12,17 +12,27 @@
 void local_search_algorithm(Polygon_2 greedypolygon, std::ofstream &outFile, int L, char* area, double threshhold){
 	// Amount of points we can switch
 	double optimal;
+	std::vector<Point_2> Points;
+	std::vector<Point_2> Examined;
+	// Make struct with Points and changes
+	// Apply
+	
 
 	while (optimal >= threshhold){
 		for(Polygon_2::Edge_const_iterator edge = greedypolygon.edges().begin(); edge != greedypolygon.edges().end(); edge++){
+			//FIRST THOUGHT, RUN THROUGH EDGES AND ADD POINTS THAT WAY
 			if (std::string(area).compare("max") == 0){
-				
+				Points.push_back(edge->start());
 			}
+			//SECOND THOUGHT, RUN THROUGH VERTICES AND ONLY KEEP EDGES FOR INITIAL SEARCH 
 			else if (std::string(area).compare("min") == 0){
+				//for(Polygon_2::Vertex_iterator vertex = polygonchain.begin(); vertex != polygonchain.end(); vertex++){
 
+				//}
 			}
 			else{
-
+				std::cerr << "Wrong arguments given! " << std::endl;
+				exit(EXIT_FAILURE);
 			}
 		}
 	}

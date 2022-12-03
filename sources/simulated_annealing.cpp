96a8ad4d4445fc0ba72924704e8f991dcf882a30
bill <gravityman12@hotmail.com>
2022-12-03 21:57:08 +0200

    update project

--- sources/simulated_annealing.cpp
+++ sources/simulated_annealing.cpp
@@ -18,26 +18,51 @@ Polygon_2* simulated_annealing::simulatedAnnealing(std::vector<Point_2> &points,
 	incremental::incrementalAlgorithm(points, initialization, 1, polygon);
 	incremental::getConvexHullPolygonFromPoints(polygon.vertices(), convexHullPolygon);
 
-	double energy = 0;
+	double energy = 0, temperature = 1, DE, R = rand() / (RAND_MAX);
+	double convexHullArea = convexHullPolygon.area();
+	double polygonArea = polygon.area();
+	double polygonAreaAfterStep, energyAfterStep;
 
 	if(max)
-		energy = points.size() * (1 - polygon.area() / convexHullPolygon.area());
+		energy = points.size() * (1 - polygonArea / convexHullArea);
 	else
-		energy = points.size() * polygon.area() / convexHullPolygon.area();
+		energy = points.size() * polygonArea / convexHullArea;
 
 	for(int i = 0; i < L; i++){
 
+		int indexOfFirstPoint;
+
 		if(strcmp(annealing, "local") == 0){
-			Tree kdTree;
-			for(Point_2 point : points)
-				kdTree.insert(point);
-			kdTree.build();
 
-			localTransitionStep(polygon, kdTree);
+			double areaRemoved = 0.0, areaAdded = 0.0;
+
+			localTransitionStep(polygon, areaRemoved, areaAdded, indexOfFirstPoint);
+
+			polygonAreaAfterStep = polygonArea - areaRemoved + areaAdded;
 		}
-		else 
+		else{
 			globalTransitionStep(polygon);
+		}
 
+		//check if step is acceptable
+		if(max){
+			energyAfterStep = points.size() * (1 - polygonAreaAfterStep / convexHullArea);
+			DE = energyAfterStep - energy;
+			if(DE > 0 || exp(-DE / temperature) > R)
+				energy = energyAfterStep;
+			else{
+				// if step is not acceptable, undo change
+				swapTwoPoints(polygon, indexOfFirstPoint);
+			}
+
+		}
+		else{
+			energyAfterStep = points.size() * polygonAreaAfterStep / convexHullArea;
+			DE = energyAfterStep - energy;
+			if(DE < 0 || exp(-DE / temperature) > R){
+
+			}
+		}
 
 	}
 
@@ -51,19 +76,26 @@ Polygon_2* simulated_annealing::simulatedAnnealingWithSubdivision(std::vector<Po
 	return nullptr;
 }
 
-void localTransitionStep(Polygon_2 &polygon, Tree &kdTree){
+void localTransitionStep(Polygon_2 &polygon, double &areaRemoved, double &areaAdded, int &indexOfFirstPoint){
+
+	//create kd-Tree
+	Tree kdTree;
+	for(Point_2 vertex : polygon.vertices())
+		kdTree.insert(vertex);
+	kdTree.build();
+
 	bool validPointSwap;
 	
 	while(1){
-
+		
 		//take a random point in polygon to swap
-		int randomPoint = 1 + (rand() % (polygon.size() - 2));
+		int randomPointIndex = 1 + (rand() % (polygon.size() - 2));
 
 		Point_2 p, q, r, s; // q and r are the points to be exchanged. p is previous to q and s is next to r
-		p = polygon[randomPoint-1];
-		q = polygon[randomPoint];
-		r = polygon[randomPoint+1];
-		s = polygon[randomPoint+2];
+		p = polygon[randomPointIndex-1];
+		q = polygon[randomPointIndex];
+		r = polygon[randomPointIndex+1];
+		s = polygon[randomPointIndex+2];
 
 		//find max and min coordianates to form the search box
 		int maxX = maxCoordinateX(p, q, r, s);
@@ -71,7 +103,8 @@ void localTransitionStep(Polygon_2 &polygon, Tree &kdTree){
 		int maxY = maxCoordinateY(p, q, r, s);
 		int minY = minCoordinateY(p, q, r, s);
 
-		//std::cout << '[' << minX << ',' << minY << "]," << '[' << maxX << ',' << minY << "]," << '[' << maxX << ',' << maxY << "]," << '[' << minX << ',' << maxY << "]," << '[' << minX << ',' << minY << ']' << std::endl;
+		// std::cout << '[' << p.x() << ',' << p.y() << "]," << '[' << q.x() << ',' << q.y() << "]," << '[' << r.x() << ',' << r.y() << "]," << '[' << s.x() << ',' << s.y() << "]," << '[' << minX << ',' << minY << ']' << std::endl;
+		// std::cout << '[' << minX << ',' << minY << "]," << '[' << maxX << ',' << minY << "]," << '[' << maxX << ',' << maxY << "]," << '[' << minX << ',' << maxY << "]," << '[' << minX << ',' << minY << ']' << std::endl;
 
 		//find the points of polygon in the box
 		std::vector<Point_2> pointsInBox;
@@ -84,8 +117,8 @@ void localTransitionStep(Polygon_2 &polygon, Tree &kdTree){
 
 		//checks if the line before p intersects with line q s
 		Point_2 t;
-		if(randomPoint - 2 >= 0)
-			t = polygon[randomPoint - 2];
+		if(randomPointIndex - 2 >= 0)
+			t = polygon[randomPointIndex - 2];
 		else
 			t = polygon[polygon.size() - 1]; 	//if p is the first point in polygon
 		Segment_2 lineT = Segment_2(t, p);
@@ -93,8 +126,8 @@ void localTransitionStep(Polygon_2 &polygon, Tree &kdTree){
 			continue;
 
 		//checks if the line after s intersects with line p r 
-		if(randomPoint + 3 < polygon.size())
-			t = polygon[randomPoint + 3];
+		if(randomPointIndex + 3 < polygon.size())
+			t = polygon[randomPointIndex + 3];
 		else
 			t = polygon[0];			//if s is the last point in polygon
 		lineT = Segment_2(s, t);
@@ -144,36 +177,25 @@ void localTransitionStep(Polygon_2 &polygon, Tree &kdTree){
 		if(!validPointSwap)
 			continue;
 
-		//remove points from polygon;
-		for(Polygon_2::Vertex_iterator vertex = polygon.begin(); vertex != polygon.end(); vertex++){
-		 	if(*vertex == q){
-				polygon.erase(vertex);
-				break;
-			}
+		//utils::polygonToPythonArray(polygon);	
+
+		//swap points
+		swapTwoPoints(polygon, randomPointIndex);	
+
+		//	calculate the area removed and area add because of swap
+		//	if triangle p, q, r has the same orientation as the polygon
+		// 	then the area of the triangle is inside the polygon
+		//	else it is outside.
+		if(polygon.orientation() == triangleOrientation(p, q, r)){
+			areaRemoved = CGAL::area(p, q, r);
+			areaAdded = CGAL::area(q, r, s);
+		}
+		else{
+			areaRemoved = CGAL::area(q, r, s);
+			areaAdded = CGAL::area(p, q, r);
 		}
 
-		for(Polygon_2::Vertex_iterator vertex = polygon.begin(); vertex != polygon.end(); vertex++){
-			if(*vertex == r){
-				polygon.erase(vertex);
-				break;
-			}
-		}
-
-		//insert points again in reverse order
-		for(Polygon_2::Vertex_iterator vertex = polygon.begin(); vertex != polygon.end(); vertex++){
-			if(*vertex == s){
-				polygon.insert(vertex, q);
-				break;
-			}
-		}
-
-		for(Polygon_2::Vertex_iterator vertex = polygon.begin(); vertex != polygon.end(); vertex++){
-			if(*vertex == q){
-				polygon.insert(vertex, r);
-				break;
-			}
-		}
-
+		indexOfFirstPoint = randomPointIndex;
 
 		// utils::polygonToPythonArray(polygon);	
 		// if(!polygon.is_simple()){
@@ -181,8 +203,32 @@ void localTransitionStep(Polygon_2 &polygon, Tree &kdTree){
 		// 	exit(1);
 		// }
 		// else
-	 	break;
+
+		break;
 	}
+}
+
+void swapTwoPoints(Polygon_2 &polygon, int indexOfFirstPoint){
+
+	// Let p, q, r, s be four consecutive points in polygon. We remove point q and place it before s
+	// indexOfFirstPoint points to q
+
+	Polygon_2::Vertex_iterator vertex = polygon.begin() + indexOfFirstPoint; 
+	Point_2 q = *vertex;
+
+	//remove q point
+	polygon.erase(vertex);
+
+	vertex = polygon.begin() + indexOfFirstPoint + 1;
+
+	// add point q after r (or before s)
+	polygon.insert(vertex, q);
+}
+
+int triangleOrientation(Point_2 &a, Point_2 &b, Point_2 &c){
+	int orientation = (b.y() - a.y()) * (c.x() - b.x()) - (b.x() - a.x()) * (c.y() - b.y());
+	if(orientation == 0) return 0;
+	return orientation < 0 ? 1 : -1;	// 1 for counter-clockwise, -1 for clockwise
 }
 
 void globalTransitionStep(Polygon_2 &polygon){

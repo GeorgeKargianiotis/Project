2710bc3327a41cffe41b5e230e0306a6bad87f0f
bill <gravityman12@hotmail.com>
2022-12-05 16:13:03 +0200

    update

---
+++ headers/local_search.hpp
@@ -0,0 +1,14 @@
+#ifndef LOCAL_SEARCH_HPP
+#define LOCAL_SEARCH_HPP
+
+#include "../headers/cgalConfig.hpp"
+
+typedef struct change Change;
+
+namespace local_search {
+	void local_search_algorithm(Polygon_2 greedypolygon, std::ofstream &outFile, int L, char* area, double threshhold);
+	int InsertPointForLS(Polygon_2 &polygon, const Point_2 &begin, const Point_2 &end, Point_2 newPoint);
+	void ApplyChanges(Polygon_2 &polygon, std::vector<Change> allchanges);
+} 
+
+#endif
